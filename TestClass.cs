77e128dde8b934a79a5595127a15ffe52c3d55a2
anmolabhishek <44404264+anmolabhishek@users.noreply.github.com>
2021-04-24 12:57:21 +0530

    Add files via upload

---
+++ TestClass.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Calculator;
+
+
+namespace N_Unit
+{
+    [TestFixture]
+    public class TestClass
+    {
+        Calc cal = new Calc();
+        
+        
+        [TestCase]
+        public void AddTest()
+        {
+            int res = cal.Add(10, 10);
+            Assert.AreEqual(20, res);
+           
+        }
+
+    }
+}
